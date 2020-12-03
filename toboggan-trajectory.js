8fa9f6285170bde4d79748261f385b394281b7bb
miami78 <miamilarry78@gmail.com>
2020-12-03 08:53:50 +0300

    day3 solution

---
+++ toboggan-trajectory.js
@@ -0,0 +1,34 @@
+const fs = require('fs'); 
+
+fs.readFile("./input2.txt", (err,data) => { 
+    if(err){ 
+      console.log(err)    
+    } else { 
+    const input = data.toString().trim().split("\n");
+      
+    function checkSlope(xx, yy) {
+        let x = 0;
+        let y = 0;
+
+        let treeCount = 0;
+
+        while (y < input.length) {    
+            if (input[y][x] === '#') {
+                treeCount++;
+            }
+
+            x = (x + xx) % input[0].length;
+            y += yy;
+        }
+        return treeCount;
+    }
+// part 1
+    console.log(checkSlope(3, 1));
+// part 2
+    console.log(checkSlope(1, 1) *
+    checkSlope(3, 1) *
+    checkSlope(5, 1) *
+    checkSlope(7, 1) *
+    checkSlope(1, 2));
+} 
+}) 
